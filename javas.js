ffe2a83e8ea3dd6bec09be861c1f44be0df10513
basa_ashok_kumar <45620529+basaashokkumar@users.noreply.github.com>
2019-04-28 14:39:10 +0530

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,16 @@
+﻿<html>
+<body>
+<script type = "text/javascript">
+function f()
+{
+var s=0;
+for(i=0;i<arguments.length;i++)
+{
+sum+=arguments[i];
+}
+document.write("sum"+sum);
+}
+f(1,3,5,7);
+</script>
+</body>
+</html>
